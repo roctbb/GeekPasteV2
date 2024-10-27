d2aee5dfdef1b7b502669c367c033011ccfa4777
roctbb <roctbb@gmail.com>
2024-10-27 23:05:30 +0300

    Add scripts to manage server and worker processes
    Introduce `stop.sh` to terminate running server and worker processes and `start.sh` to initiate them. `restart.sh` combines these actions to first stop and then start the processes, ensuring a smooth restart mechanism.

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-python3 paste_server.py >> /dev/null 2>&1 & disown
-python3 -m celery -A paste_celery.celery worker --loglevel=info  >> /dev/null 2>&1 & disown
+nohup python3 paste_server.py > /dev/null 2>&1 &
+nohup python3 -m celery -A paste_celery.celery worker --loglevel=info > /dev/null 2>&1 &
