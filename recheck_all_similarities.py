c1b3f9b5503ccec24618d1c1dcd2a1808b7cdceb
roctbb <roctbb@gmail.com>
2025-09-23 18:44:43 +0300

    Add recheck

--- recheck_all_similarities.py
+++ recheck_all_similarities.py
@@ -6,23 +6,20 @@ import checker
 with app.app_context():
     codes = get_all_codes()
 
+    db.session.query(similarities_table).delete()
+    db.session.commit()
+
+
     for code in tqdm(codes):
         if not code.similarity_checked:
-            if code.task and code.task.bypass_similarity_check:
-                print(f"Bypass code ID {code.id}")
-                continue
-
-            print(f"Checking code ID {code.id}")
-
             for code2 in codes:
                 if code2.id == code.id:
                     continue
 
                 n = checker.similarity(code.code, code2.code)
+                code.similarity_checked = True
 
                 if n >= SIMILARITY_LEVEL:
                     save_similarity(code, code2, n)
-
-            code.similarity_checked = True
 
     db.session.commit()
